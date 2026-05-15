8359c883715d030585ff4e6f158b26d93ff23a55
Artjoms <artv20022@gmail.com>
2026-05-16 00:16:51 +0300

    feat: update 8086sim

--- 8086sim/8086sim.c
+++ 8086sim/8086sim.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h> /* strcat */
 #include <stdlib.h> /* strtol */
+#include <stdint.h>
 
 /* 8086 simulator */
 /*
@@ -21,6 +22,14 @@ const char *byte_to_binary(int x)
 	{
 		strcat(b, ((x & z) == z) ? "1" : "0");
 	}
+
+	return b;
+}
+
+char loadInstructionByte(FILE *file, char *buffer, int *i)
+{
+	char b = getc(file);
+	buffer[*i++] = b;
 
 	return b;
 }
@@ -51,7 +60,7 @@ int main(int argc, char *argv[])
 	FILE *fileOut = fopen(outFileName, "w");
 
 	unsigned char buffer[1024] = {'\n'};
-	char byte1, byte2;
+	char byte1, byte2, byte3, byte4, byte5, byte6;
 	int i = 0;
 
 	char *opcodes[64],
@@ -65,60 +74,151 @@ int main(int argc, char *argv[])
 	opcodes[11] = "mov";
 
 	unsigned int opcode = 0,
-				  d = 0,
-				  w = 0,
-				  mod = 0,
-				  reg = 0,
-				  rm = 0,
-				  data = 0;
+				 d = 0,
+				 w = 0,
+				 mod = 0,
+				 reg = 0,
+				 rm = 0,
+				 data1 = 0,
+				 data2 = 0;
 
-	unsigned int opcodeMask = 0b11111111,
-				  dMask = 0,
-				  wMask = 0,
-				  modMask = 0,
-				  regMask = 0,
-				  rmMask = 0,
-				  dataMask = 0;
+	uint64_t opcodeMask = 0b11111111,
+			 dMask = 0,
+			 wMask = 0,
+			 modMask = 0,
+			 regMask = 0,
+			 rmMask = 0,
+			 data1Mask = 0,
+			 data2Mask = 0;
 
 	fprintf(fileOut, "bits 16\n");
 
+	uint64_t instructions[2][2][13] = {
+		// MOV
+
+		// register/memory to/from register
+		{
+			// opcode
+			{0b100010},
+			{
+				// opcode mask - 0
+				0b100010000000000000000000000000000000000000000000,
+				// d mask - 1
+				0b000000100000000000000000000000000000000000000000,
+				// w mask - 2
+				0b000000010000000000000000000000000000000000000000,
+				// mod mask - 3
+				0b000000001100000000000000000000000000000000000000,
+				// reg mask - 4
+				0b000000000011100000000000000000000000000000000000,
+				// r/m mask - 5
+				0b000000000000011100000000000000000000000000000000,
+				// disp-lo mask - 6
+				0b000000000000000011111111000000000000000000000000,
+				// disp-hi mask - 7
+				0b000000000000000000000000111111110000000000000000,
+				// data1 mask - 8
+				0b000000000000000000000000000000000000000000000000,
+				// data2 mask - 9
+				0b000000000000000000000000000000000000000000000000,
+				// SR mask - 10
+				0b000000000000000000000000000000000000000000000000,
+				// addr-lo mask - 11
+				0b000000000000000000000000000000000000000000000000,
+				// addr-hi mask - 12
+				0b000000000000000000000000000000000000000000000000,
+
+			},
+		},
+		// immidiate to register
+		{
+			// opcode
+			{0b1011},
+			{
+				// opcode mask - 0
+				0b101100000000000000000000000000000000000000000000,
+				// d mask - 1
+				0b000000000000000000000000000000000000000000000000,
+				// w mask - 2
+				0b000010000000000000000000000000000000000000000000,
+				// mod mask - 3
+				0b000000000000000000000000000000000000000000000000,
+				// reg mask - 4
+				0b000001110000000000000000000000000000000000000000,
+				// r/m mask - 5
+				0b000000000000000000000000000000000000000000000000,
+				// disp-lo mask - 6
+				0b000000000000000000000000000000000000000000000000,
+				// disp-hi mask - 7
+				0b000000000000000000000000000000000000000000000000,
+				// data1 mask - 8
+				0b000000001111111100000000000000000000000000000000,
+				// data2 mask - 9
+				0b000000000000000011111111000000000000000000000000,
+				// SR mask - 10
+				0b000000000000000000000000000000000000000000000000,
+				// addr-lo mask - 11
+				0b000000000000000000000000000000000000000000000000,
+				// addr-hi mask - 12
+				0b000000000000000000000000000000000000000000000000,
+			}}};
+
 	while (1)
 	{
-		byte1 = getc(file);
-		byte2 = getc(file);
-		if (byte1 == EOF || byte2 == EOF)
+		byte1 = loadInstructionByte(file, buffer, &i);
+		if (byte1 == EOF)
 		{
 			break;
 		}
 
-		printf("bytes: ");
-		printf(" %s ", byte_to_binary(byte1));
-		printf(" %s ", byte_to_binary(byte2));
-		buffer[i++] = byte1;
-		buffer[i++] = byte2;
-		printf("\n\n");
+		opcode = ((unsigned char)byte1);
 
-		opcode = ((unsigned char)byte1 & opcodeMask);
 
 		// update masks depending on opcode retrieved
-		if ((opcode >> 2) == 0b0100010)
+		if ((opcode >> 2) == instructions[0][0][0])
 		{
+
 			// register / memory to / from register
-			opcodeMask = 0b11111100;
-			dMask = 0b00000010;
-			wMask = 0b00000001;
-			modMask = 0b11000000;
-			regMask = 0b00111000;
-			rmMask = 0b00000111;
+			opcodeMask = instructions[0][1][0];
+			dMask = instructions[0][1][1];
+			wMask = instructions[0][1][2];
+			modMask = instructions[0][1][3];
+			regMask = instructions[0][1][4];
+			rmMask = instructions[0][1][5];
 
-			opcode = ((unsigned char)byte1 & opcodeMask) >> 2;
+			byte2 = loadInstructionByte(file, buffer, &i);
 
-			d = ((unsigned char)byte1 & dMask) >> 1;
-			w = ((unsigned char)byte1 & wMask);
+			uint64_t instruction = ((uint64_t)(uint8_t)byte1 << 40) |
+								   ((uint64_t)(uint8_t)byte2 << 32);
 
-			mod = ((unsigned char)byte2 & modMask) >> 6;
-			reg = ((unsigned char)byte2 & regMask) >> 3;
-			rm = ((unsigned char)byte2 & rmMask);
+			mod = (instruction & modMask) >> 38;
+
+			if (mod == 0b01 || mod == 0b10)
+			{
+				byte3 = loadInstructionByte(file, buffer, &i);
+				byte4 = loadInstructionByte(file, buffer, &i);
+				uint64_t instruction = ((uint64_t)(uint8_t)byte1 << 40) |
+									   ((uint64_t)(uint8_t)byte2 << 32) |
+									   ((uint64_t)(uint8_t)byte3 << 24) |
+									   ((uint64_t)(uint8_t)byte4 << 16);
+			}
+
+			printf("instruction: ");
+			printf("%s ", byte_to_binary(byte1));
+			printf("%s ", byte_to_binary(byte2));
+			printf("%s ", byte_to_binary(byte3));
+			printf("%s ", byte_to_binary(byte4));
+			printf("%s ", byte_to_binary(0));
+			printf("%s ", byte_to_binary(0));
+			printf("\n");
+
+			opcode = (instruction & opcodeMask) >> 42;
+
+			d = (instruction & dMask) >> 41;
+			w = (instruction & wMask) >> 40;
+
+			reg = (instruction & regMask) >> 35;
+			rm = (instruction & rmMask) >> 32;
 
 			printf("register / memory to / from register\n");
 
@@ -131,7 +231,6 @@ int main(int argc, char *argv[])
 
 			printf("\n");
 			printf("decoded: \n");
-			printf("opcode: %s\n", opcodes[opcode]);
 
 			// Direction
 			if (d)
@@ -181,30 +280,40 @@ int main(int argc, char *argv[])
 
 			printf("================\n\n");
 		}
-		else if ((opcode >> 4) == 0b1011)
+		else if ((opcode >> 4) == instructions[1][0][0])
 		{
 			// immidiate to register
-			opcodeMask = 0b1111000000000000;
-			dMask = 0b0000000000000000;
-			wMask = 0b0000100000000000;
-			modMask = 0b0000000000000000;
-			regMask = 0b0000011100000000;
-			rmMask = 0b0000000000000000;
-			dataMask = 0b0000000011111111;
+			byte2 = loadInstructionByte(file, buffer, &i);
+			byte3 = loadInstructionByte(file, buffer, &i);
 
-			int instruction = (byte1 << 8) | byte2;
+			// register / memory to / from register
+			opcodeMask = instructions[1][1][0];
+			dMask = instructions[1][1][1];
+			wMask = instructions[1][1][2];
+			modMask = instructions[1][1][3];
+			regMask = instructions[1][1][4];
+			rmMask = instructions[1][1][5];
+			data1Mask = instructions[1][1][8];
+			data2Mask = instructions[1][1][9];
 
-			opcode = (instruction & opcodeMask) >> 12;
+			uint64_t instruction = ((uint64_t)(uint8_t)byte1 << 40) |
+								   ((uint64_t)(uint8_t)byte2 << 32) |
+								   (uint64_t)(uint8_t)byte3 << 24;
 
-			d = (instruction & dMask);
-			w = (instruction & wMask) >> 11;
+			printf("instruction: ");
+			printf("%s ", byte_to_binary(byte1));
+			printf("%s ", byte_to_binary(byte2));
+			printf("%s ", byte_to_binary(byte3));
+			printf("\n");
 
-			mod = (instruction & modMask);
-			reg = (instruction & regMask) >> 8;
-			rm = (instruction & rmMask);
-			data = (instruction & dataMask);
-
-			printf("immidiate to register/memory opcode: %s\n", byte_to_binary(opcode));
+			opcode = (instruction & opcodeMask) >> 44;
+			d = 0;
+			w = (instruction & wMask) >> 43;
+			mod = 0;
+			reg = (instruction & regMask) >> 40;
+			rm = 0;
+			data1 = (instruction & data1Mask) >> 32;
+			data2 = (instruction & data2Mask) >> 24;
 
 			printf("opcode: %s\n", byte_to_binary(opcode));
 			printf("d: %s\n", byte_to_binary(d));
@@ -212,10 +321,11 @@ int main(int argc, char *argv[])
 			printf("mod: %s\n", byte_to_binary(mod));
 			printf("reg: %s\n", byte_to_binary(reg));
 			printf("rm: %s\n", byte_to_binary(rm));
+			printf("data1: %s\n", byte_to_binary(data1));
+			printf("data2: %s\n", byte_to_binary(data2));
 
 			printf("\n");
 			printf("decoded: \n");
-			printf("opcode: %s\n", opcodes[opcode]);
 
 			// Direction
 			if (d)
@@ -260,8 +370,8 @@ int main(int argc, char *argv[])
 				printf("rm: %s\n", regs[rm]);
 
 			printf("full instruction: \n");
-			printf("%s %s, %d\n", opcodes[opcode], regs[reg], data);
-			fprintf(fileOut, "%s %s, %d\n", opcodes[opcode], regs[reg], data);
+			printf("%s %s, %d\n", opcodes[opcode], regs[reg], data1);
+			fprintf(fileOut, "%s %s, %d\n", opcodes[opcode], regs[reg], data1);
 
 			printf("================\n\n");
 		}
